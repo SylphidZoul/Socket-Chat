c2d5bd4b982b2e90e28f4c045790a4b0c0b7b14c
Juan Cruz Cardozo <60722288+SylphidZoul@users.noreply.github.com>
2020-12-15 22:24:04 -0300

    Update usuarios.js

--- server/classes/usuarios.js
+++ server/classes/usuarios.js
@@ -18,7 +18,6 @@ class Usuarios {
   }
 
   getPersona(id){
-    // filter devuelve una nueva array construida a partir de que la condicion sea true, por lo qe tambien debemos ponerle el indice
     let persona = this.personas.filter( persona =>  persona.id === id)[0]
 
     return persona;
@@ -39,8 +38,6 @@ class Usuarios {
   borrarPersona(id){
 
     let personaBorrada = this.getPersona(id)
-    // Lo mismo qe arriba, creo una nueva array a partir de qe la condicion sea true, en este caso va a excluir de la array
-    // la persona que estoy buscando
     this.personas = this.personas.filter( persona => persona.id !== id)
 
     return personaBorrada;
